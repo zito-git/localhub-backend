61560ef7450d8aa00df1447cbf306601585a92b5
zito-git <iam@zito.kr>
2026-07-14 17:49:51 +0900

    cors

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from database import engine
 from models import Base
 from routers import posts
 
+# CORS
+from fastapi.middleware.cors import CORSMiddleware
+
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -16,6 +20,16 @@ app = FastAPI(
 구미·경북권 관광 데이터를 조회하는 REST API입니다.
 """,
 )
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 출처 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용(GET, POST, PUT, DELETE 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
 
 app.include_router(posts.router)
 app.include_router(data.router)
